07b6358f537c67d188540ccca6fde9177a26a93a
aby <abysamassa@yahoo.fr>
2024-12-18 15:35:28 +0100

    2éme étapes de l'exercice mastermind

--- Masterminds.py
+++ Masterminds.py
@@ -2,3 +2,8 @@ import random
 Maximun_Length = 4
 Maximun_of_tries = 6
 Authorized_color = ["rouge","violet","bleu""jaune"]
+def Generator_Secret_Pass():
+        colors= random.choices(Authorized_color,k=Maximun_Length)
+        return colors
+The_conbination= Generator_Secret_Pass()
+print(f"la combinaison secréte est {The_conbination}") 
