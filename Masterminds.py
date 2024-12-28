2f5f3204a26218be4c97209fe3c31a0cbb9bbb5c
aby <abysamassa@yahoo.fr>
2024-12-28 03:09:57 +0100

    implémentation de la fonction Main_game pour établir les régles principales du jeu de plus l'appel de la fonction anwsers_game a été repositionnée a l'intérieur de la fonction Main_game

--- Masterminds.py
+++ Masterminds.py
@@ -1,26 +1,27 @@
 import random
-Maximun_Length = 4
-Maximun_of_tries = 10
-Authorized_color = ["rouge","violet","bleu","jaune"]
-def Generator_Secret_Pass():
-        colors= random.choices(Authorized_color,k=Maximun_Length)
-        return colors
-The_conbination= Generator_Secret_Pass()
-print(f"la combinaison secréte est {The_conbination}") 
 
-def anwsers_gamers():
+Maximun_Length = 4
+Authorized_color = ["rouge", "violet", "bleu", "jaune"]
+
+def Generator_Secret_Pass():
+    colors = random.choices(Authorized_color, k=Maximun_Length)
+    return colors
+
+def anwsers_gamers(The_conbination):
     secret_code= input("entrez le code secret").split()
     for color in secret_code:
          if not color.isalpha():
             print("Erreur veuillez entrer uniquement des caractéres.")
-            return anwsers_gamers() 
+            return False
     if len(secret_code) != Maximun_Length:
        print("erreur le code fait plus de 4 couleurs")
+       return False
     elif secret_code== The_conbination:
          print ("bravo tu a trouvé le code secret")
     else:
         print ("ce n'est pas le bon code")
         solution_verif (secret_code, The_conbination)
+
 
 def solution_verif (secret_code, The_conbination):
     box_good_place=0
@@ -38,5 +39,18 @@ def solution_verif (secret_code, The_conbination):
          
     print(f"{box_good_place} couleurs bien placée(s), {box_bad_place} couleurs mal placée(s).")
  
-anwsers_gamers()
 
+def Main_game():
+    The_conbination = Generator_Secret_Pass()
+    number_of_tries = 0
+    Maximun_of_tries = 10
+    game_won = False
+    while number_of_tries < Maximun_of_tries:
+        if anwsers_gamers(The_conbination):
+            game_won = True
+            break
+        number_of_tries += 1
+    if not game_won:
+        print("Tu as perdu. Retente ta chance!")
+
+Main_game()
